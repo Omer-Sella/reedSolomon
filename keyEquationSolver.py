cf05d5d2bb7c90a871e88ac40adc11a22c9b3d66
Omer Sella <o.sella@imperial.ac.uk>
2024-03-22 11:42:45 +0000

    KES is working

--- keyEquationSolver.py
+++ keyEquationSolver.py
@@ -18,59 +18,41 @@ def keyEquationSolver(polynomialClass, galoisElement, syndromes):
      for k in range(len(syndromes)):
          K = k + 1
          discrepancy = galoisElement(value = syndromes[k])
+         #print("Disc value == ")
+         #print(discrepancy.value)
          for i in range(L):
-             print("i == " + str(i))
-             discrepancy = discrepancy + cX.coefficients[i].times((galoisElement(syndromes[K-i])))
+             #print("k is:" + str(k))
+             #print("i is : "+ str(i))
+             diff =  cX.coefficients[(i+1)].times((galoisElement(syndromes[k-(i+1)])))
+             discrepancy = discrepancy + diff
+             #print("Diff value == ")
+             #print(diff.value)
          # ???discrepancy = syndromes + discrepancy
          if discrepancy.isZero():
              l = l + 1
          else:
              if (2 * L) >= K: #Note the fix k replaced with K
+                 #print("****************")    
                  cX = cX - (pX.timesScalar((discrepancy.times(oldDiscrepancy.inverse())))).lift(l)
                  l = l + 1
              else:
+                 
                  tX = cX
                  cX = cX - (pX.timesScalar((discrepancy.times(oldDiscrepancy.inverse())))).lift(l)
                  L = K - L
                  pX = tX
                  oldDiscrepancy = discrepancy
                  l = 1
-         print("| K | syndromes[k] | disc | l  | L   | old discrepancy |")
-         print("| %d | %d            | %d    | %d  | %d   | %d              |" % (K, syndromes[k], discrepancy.value, l, L,  oldDiscrepancy.value))
+         #print("| K | syndromes[k] | disc | l  | L   | old discrepancy |")
+         #print("| %d | %d            | %d    | %d  | %d   | %d              |" % (K, syndromes[k], discrepancy.value, l, L,  oldDiscrepancy.value))
          
-         print("c(x) == ")
-         cX.printValues()
-         print("p(x) == ")
-         pX.printValues()
+         #print("c(x) == ")
+         #cX.printValues()
+         #print("p(x) == ")
+         #pX.printValues()
          
         
      return cX
 
-        
-def berlekampMassey():
-    #An implementation following Richard E. Blahut page 187
-    L = 0
-    r = 0
-    lambdaX = 1
-    bX = 1
-    return lambdaX
 
-def test_keyEquationSolver():
-    #Testing the Todd K. Moon version using example on page 281 (laboratory 6 ex. 2)
-    # As well as table 6.5 on page 259
-    #|K | S[k] | discrepancyK | cX          | L | pX   | l | oldDiscrepancy
-    #|1 | 1    | 1            | 1+x         | 1 | 1    | 1 | 1
-    #|2 | 1    | 0            | 1+x         | 1 | 1    | 1 | 1
-    #|3 | 1    | 0            | 1+x         | 1 | 1    | 1 | 1
-    #|4 | 0    | 1            | 1+x+x^3     | 3 | 1+x  | 1 | 1
-    #|5 | 1    | 0            | 1+x+x^3     | 3 | 1+x  | 1 | 1
-    #|6 | 0    | 0            | 1+x+x^3     | 3 | 1+x  | 1 | 1
-    #|7 | 0    | 0            | 1+x+x^3     | 3 | 1+x  | 1 | 1
-
-    import numpy as np
-    #r = np.array([0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0]) #This is equivalent to r(x) = x + x^3 + x^8
-    syndromes = np.array([ 1, 1, 1 , 0, 1 , 0, 0])
-    #syndromes = np.array([ 0,0,1,0,1,1,1])
-    cX = keyEquationSolver(polynomialClass, galoisElement, syndromes)
-    cX.printValues()
-    assert(np.all(cX.coefficients == np.array([1,1,0,1])))
+    
---
+++ test_keyEquationSolver.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 11:41:12 2024
+
+@author: Omer
+"""
+from keyEquationSolver import *
+
+def test_keyEquationSolver():
+    #Testing the Todd K. Moon version using example on page 281 (laboratory 6 ex. 2)
+    # As well as table 6.5 on page 259
+    #|K | S[k] | discrepancyK | cX          | L | pX   | l | oldDiscrepancy
+    #|1 | 1    | 1            | 1+x         | 1 | 1    | 1 | 1
+    #|2 | 1    | 0            | 1+x         | 1 | 1    | 1 | 1
+    #|3 | 1    | 0            | 1+x         | 1 | 1    | 1 | 1
+    #|4 | 0    | 1            | 1+x+x^3     | 3 | 1+x  | 1 | 1
+    #|5 | 1    | 0            | 1+x+x^3     | 3 | 1+x  | 1 | 1
+    #|6 | 0    | 0            | 1+x+x^3     | 3 | 1+x  | 1 | 1
+    #|7 | 0    | 0            | 1+x+x^3     | 3 | 1+x  | 1 | 1
+
+    import numpy as np
+    #r = np.array([0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0]) #This is equivalent to r(x) = x + x^3 + x^8
+    syndromes = np.array([ 1, 1, 1 , 0, 1 , 0, 0])
+    #syndromes = np.array([ 0,0,1,0,1,1,1])
+    cX = keyEquationSolver(polynomialClass, galoisElement, syndromes)
+    cX.printValues()
+    assert(cX.coefficients[0].value == 1)
+    assert(cX.coefficients[1].value == 1)
+    assert(cX.coefficients[2].value == 0)
+    assert(cX.coefficients[3].value == 1)
